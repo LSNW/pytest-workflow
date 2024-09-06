cf594c0edb31966a10a172de1066fed3e8ea307f
Winston <winstonl2002@gmail.com>
2024-09-06 17:26:11 -0400

    change failing test

---
+++ tests/test_basic2.py
@@ -0,0 +1,11 @@
+from arithmetic.basic import sum, diff, mult
+
+def test_sum():
+    assert sum(1,5) == 6
+
+def test_diff():
+    assert diff(3,3) == 0
+
+def test_mult():
+    assert mult(-5, 6) == -30
+
